4a18d819274da9f1a49ee252416f79d60fbdfc0b
Julija <julija@Julijas-MacBook-Pro.local>
2025-02-25 20:45:07 +0100

    final

--- src/Footer.js
+++ src/Footer.js
@@ -9,20 +9,26 @@ const Footer = () => {
       </div>
       
       <div className="footer-links">
+      <h4>Nav</h4>
         <ul>
-          <li><a href="/privacy">Privacy Policy</a></li>
-          <li><a href="/terms">Terms of Service</a></li>
-          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/reservations">Reservations</a></li>
+          <li><a href="/order">Order Online</a></li>
+          <li><a href="/login">Login</a></li>
         </ul>
       </div>
 
       <div className="footer-address">
+        <h4>Address</h4>
         <p>123 Flavor Street</p>
         <p>Chicago, IL 60601</p>
         <p>+1 (312) 555-7890</p>
       </div>
 
       <div className="footer-social">
+      <h4>Follow Us</h4>
         <a href="https://facebook.com">Facebook</a>
         <a href="https://instagram.com">Instagram</a>
       </div>
